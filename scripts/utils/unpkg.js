f1d78923cf73cc8cc28ef1c7a7c28294370c4321
Zababurin <s.zababurin.v@gmail.com>
2021-03-22 00:21:05 +0300

    update

--- scripts/utils/unpkg.js
+++ scripts/utils/unpkg.js
@@ -1,7 +1,7 @@
 const fetch = require('isomorphic-fetch');
 
 function getMetadata(packageName, version) {
-  return fetch(`https://unpkg.com/${packageName}@${version}/?meta`, {
+  return fetch(`https://github.com/zababurinsv/${packageName}@${version}/?meta`, {
     method: 'GET'
   }).then(res => res.json());
 }
